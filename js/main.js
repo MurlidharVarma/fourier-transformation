e9580445d15d041d4d4786b236a6c730d060b2ef
Murlidhar Varma <murlidhar.varma@cummins.com>
2020-05-24 13:01:09 +0530

    multiple circle support

--- js/main.js
+++ js/main.js
@@ -1,5 +1,9 @@
 let width = 0;
 let height = 0;
+let diagWidth = width / 5;
+let diagHeight = 200;
+let noOfCircles = 10;
+let skip = 2;
 
 let time = 0;
 
@@ -7,47 +11,77 @@ ySignal = [];
 
 function setup() {
   width = windowWidth;
-  height = windowHeight;
+
+  diagWidth = width / 4;
+  diagHeight = width/10 * (4 / (1 * PI));
+
+  height = (diagHeight * noOfCircles * 1.5);
+
   createCanvas(width,height);
+  for (let c=1; c <= noOfCircles; c+=skip){
+    ySignal[c]=[];
+  }
 }
 
 function draw() {
   background(0);
 
-  translate(width/4, height/2)
-  stroke(255, 100);
-  strokeWeight(1);
-  noFill();
-  let radius = width/10;
-  ellipse(0, 0, radius*2);
 
-  push()
-    stroke('yellow');
-    strokeWeight(2);
-    let x = radius * cos(time);
-    let y = radius * sin(time);
-    ellipse(x,y,radius*0.1);
-  
-    line(0, 0, x, y);
-    strokeWeight(0.5);
-    line(x, y, 200, y);
-
-  pop()
-
-  ySignal.unshift(y)
-
-  push()
-    stroke('green');
-    strokeWeight(2);
-    beginShape();
-    for (let i=0; i < ySignal.length; i++){
-      point(i+200,ySignal[i]);
-    }
-    endShape();
-  pop()
-
-  if(ySignal.length> width/4){
-    ySignal.pop();
+  for (let c=1; c <= noOfCircles; c+=skip){
+    let h = 100+(diagHeight*c*2.5/(skip));
+    textSize(28);
+    fill('blue')
+    text(c,10,h-diagHeight)
+    drawEpiCycle(c, diagWidth, h, c*2+1);
   }
+  
   time += 0.05;
+}
+
+function drawEpiCycle(idx, posX, posY, maxFreq){
+  let centerX = 0;
+  let centerY = 0;
+  let x=0, y=0;
+  for (let freq=1; freq < maxFreq; freq+=2){
+    push()
+      translate(posX, posY);
+
+      stroke(255, 100);
+      strokeWeight(1);
+      noFill();
+      let radius = width/10 * (4 / (freq * PI));
+      ellipse(centerX, centerY, radius*2);
+
+      push();
+        stroke('yellow');
+        x += radius * cos(freq * time);
+        y += radius * sin(freq * time);
+        line(centerX, centerY, x, y);
+
+        strokeWeight(5);
+        point(x,y);
+      pop();
+
+      centerX = x;
+      centerY = y;
+    pop();
+  }
+  ySignal[idx].unshift(y)
+
+    push();
+      translate(posX, posY);
+      stroke('green');
+      strokeWeight(2);
+      noFill();
+      beginShape();
+      for (let i=0; i < ySignal[idx].length; i++){
+        vertex(i+200,ySignal[idx][i]);
+      }
+      line(x, y, 200, ySignal[idx][0]);
+      endShape();
+    pop();
+
+  if(ySignal[idx].length> width/4){
+    ySignal[idx].pop();
+  }
 }
